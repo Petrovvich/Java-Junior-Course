0dbf876fd147c9e0ccd992cf1fb48b377af24f2c
Petrovvich <alexromantrue@gmail.com>
2017-08-20 00:27:34 +0700

    Изучение гит

--- src/lesson_1_tasks/Tasks.java
+++ src/lesson_1_tasks/Tasks.java
@@ -3,5 +3,6 @@ package lesson_1_tasks;
 public class Tasks {
     public static void main(String[] args) {
         System.out.println("Тестирование гит ч.2");
+        System.out.println();
     }
 }
